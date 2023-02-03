d0efb92840ac9dbafada74a5ef2487f6abc88dc5
Milo C <75094742+whotf1@users.noreply.github.com>
2023-02-02 18:43:47 -0600

    added basic routing for the pages :), also added cLog becuase it looks nice

--- main/server.js
+++ main/server.js
@@ -18,12 +18,35 @@
 //GET/api/notes should read the db.json fiile and return all saved notes as JSON
 //POST/api/notes should recieve a new note, add it to the db,and return the new note to client
 //need npm package to add ids to notes
-
+const { clog } = require('./middleware/clog');
 
 const FS = require("fs");
 const express = require("express");
+const path = require("path");
+const app = express();
 
 
+const PORT = process.env.port || 8080;
+app.use(clog);
 
+app.use(express.static('public'));
+
+//rout for homepage
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname,"/public/pages/index.html"));
+});
 
 //route for /notes before anything else
+
+app.get("/notes", (req,res) => {
+  res.sendFile(path.join(__dirname, "/public/pages/notes.html"));
+});
+
+//route for 404 page
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/pages/404.html"))
+});
+
+app.listen(PORT, () =>{
+  console.log(`app now listening on port: http://localhost:${PORT}`)
+});
